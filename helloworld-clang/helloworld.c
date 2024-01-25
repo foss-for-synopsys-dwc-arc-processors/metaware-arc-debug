a10e001df9cd100b91d16ffda4056dfc3e8718e1
Justin You <justin.you@synopsys.com>
2024-01-25 18:34:10 +0800

    windows version for clang + lldb, some progress

---
+++ helloworld-clang/helloworld.c
@@ -0,0 +1,16 @@
+
+
+    int step = 3;
+    int total = 0;
+
+int main(int args, char* argv[])
+{
+
+
+    while( -1 ) {
+        total = total + step;
+        step = step * 2;
+    }
+
+    return 0;
+}
